b1460c98cb1ab31fb9a68297c2c021e58ddb940e
SentientBagel <jhnelson.contact@gmail.com>
2022-02-07 01:58:01 -0500

    hhhhh

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-/*function expandTerms() {
-    var x = document.getElementById("terms-text");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}*/
-
-function checkAll2() {
-    document.getElementById("checkbox").checked = true;
+//checks off all checkboxes when hovering over the continue button
+function checkAll(){
+  var x = document.getElementsByClassName("cb");
+  for(var thing of x){
+    thing.checked = true;
+  }
 }
 
-function checkAll1() {
-    //document.getElementById("checkbox").checked = true;
-    var x = document.querySelectorAll('input[type="checkbox"]:checked');
-    for (var checkbox of x) {
-        checkbox.checked = true;
-    } 
-}
-
-function checkAll(source) {
-    checkboxes = document.getElementById("checkbox").checked
-    for(var checkbox in checkboxes)
-    checkbox.checked = source.checked;
-}
-
+//toggles hiding or showing the terms and conditions text
 function toggleHide(){
   var x = document.getElementById("terms-text");
   x.classList.toggle('hide');
 }
 
+//alerts the user when they uncheck a checkbox
 function checkChanged(){
   // Let's make this as annoying as possible
-  if(document.getElementById("checkbox").checked == false){
+  if(document.getElementById("cb1").checked == false || document.getElementById("cb2").checked == false
+  || document.getElementById("cb3").checked == false || document.getElementById("cb4").checked == false
+  || document.getElementById("cb5").checked == false || document.getElementById("cb6").checked == false
+  || document.getElementById("cb7").checked == false || document.getElementById("cb8").checked == false
+  || document.getElementById("cb9").checked == false || document.getElementById("cb10").checked == false) {
     alert("Are you sure you want to do that? If so, click OK.");
   }
 }
